e6b8d739b3da3af25fc585a06cc8983304fa8710
Swastikyadav <swastikyadav.dev@gmail.com>
2020-12-09 13:17:42 +0530

    implemented registration controller and added logout and logged_in action in session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  include CurrentUserConcern
+
   def create
     user = User
             .find_by(email: params["user"]["email"])
@@ -8,7 +10,20 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       render json: { status: :created, logged_in: true, user: user }
     else
-      render json: { status: 401 }
+      render json: { status: :unprocessable_entity, errors: user.errors.full_messages }
+    end
+  end
+
+  def destroy
+    reset_session
+    render json: { status: 200, logged_out: true }
+  end
+
+  def logged_in
+    if @current_user
+      render json: { logged_in: true, user: @current_user }
+    else
+      render json: { logged_in: false }
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :sessions, only: [:create]
+  resources :sessions, only: [:create, :destroy] do
+    get "logged_in", on: :member
+  end
+  resources :registrations, only: [:create]
   root to: "static#home"
 end
