375e04d99b9a44dc0ad829fb5c0e49a971b36986
Swastikyadav <swastikyadav.dev@gmail.com>
2020-12-09 13:29:33 +0530

    configured session_store to work in all environment

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: "_daily_manifest_app", domain: "dailymanifest-api.herokuapp.com"
+if Rails.env == "production"
+  Rails.application.config.session_store :cookie_store, key: "_daily_manifest_app", domain: "dailymanifest-api.herokuapp.com"
+else
+  Rails.application.config.session_store :cookie_store, key: "_daily_manifest_app"
+end
