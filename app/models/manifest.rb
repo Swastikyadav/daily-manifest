f53ef90077632c9bda5d46b31e6e457744b1841c
Swastikyadav <swastikyadav.dev@gmail.com>
2020-12-10 20:17:54 +0530

    added validated true option to has_one

--- app/models/manifest.rb
+++ app/models/manifest.rb
@@ -1,6 +1,6 @@
 class Manifest < ApplicationRecord
   belongs_to :user
-  has_one :reading, :dependent => :destroy
+  has_one :reading, :dependent => :destroy, :validate => true
 
   validates :day, presence: true
   validates :date_of_manifest, presence: true
