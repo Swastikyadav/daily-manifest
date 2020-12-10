18fe5a4e03564b55a3325818edf408e647ea53b0
Swastikyadav <swastikyadav.dev@gmail.com>
2020-12-10 12:31:15 +0530

    added routes and controller for manifest

---
+++ app/controllers/manifests_controller.rb
@@ -0,0 +1,48 @@
+class ManifestsController < ApplicationController
+  before_action :load_user
+  before_action :load_manifest, except: [:create]
+
+  def show
+    render json: { manifest: @manifest }
+  end
+
+  def create
+    @manifest = @user.manifests.new(manifest_params)
+    if @manifest.save
+      render json: { status: :ok, manifest: @manifest }
+    else
+      render json: { status: unprocessable_entity, errors: @manifest.errors.full_messages }
+    end
+  end
+
+  def update
+    if @manifest.update(manifest_params)
+      render json: { status: :ok, updated_manifest: @manifest }
+    else
+      render json: { status: :unprocessable_entity, errors: @manifest.errors.full_messages }
+    end
+  end
+
+  def destroy
+    if @manifest.destroy
+      render json: { status: :ok, destroyed_manifest: @manifest }
+    else
+      render json: { status: :bad_request, errors: @manifest.errors.full_messages }
+    end
+  end
+
+  private
+
+    def load_user
+      @user = User.find_by!(id: params[:registration_id])
+    end
+
+    def load_manifest
+      @manifest = @user.manifests.find_by!(id: params[:id])
+    end
+
+    def manifest_params
+      params.require(:manifest)
+        .permit(:day, :date_of_manifest)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resource :sessions, only: [:create, :destroy] do
     get "logged_in", on: :member
   end
-  resources :registrations, only: [:create, :destroy]
+  resources :registrations, only: [:create, :destroy] do
+    resources :manifests, only: [:create, :show, :update, :destroy]
+  end
   root to: "static#home"
 end
