d7c1c7916ea3d928c099a9c0c6324c81c8a471ed
Swastikyadav <swastikyadav.dev@gmail.com>
2020-12-12 13:32:34 +0530

    allow delete method in cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,11 +1,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "http://localhost:3000"
-    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :options, :head], credentials: true
+    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :options, :head, :delete], credentials: true
   end
 
   allow do
     origins "https://dailymanifest-api.herokuapp.com"
-    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :options, :head], credentials: true
+    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :options, :head, :delete], credentials: true
   end
 end
