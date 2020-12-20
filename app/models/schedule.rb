9133dda103ebe81f41bb201638377d123f3310a3
Swastikyadav <swastikyadav.dev@gmail.com>
2020-12-20 23:45:23 +0530

    changed time_entries length from 12 to 13

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,6 +1,6 @@
 class Schedule < ApplicationRecord
   belongs_to :manifest
 
-  validates :time_entry, length: { is: 12 }
+  validates :time_entry, length: { is: 13 }
   validates_with SingleRecordValidator, on: :create, name: "schedule"
 end
